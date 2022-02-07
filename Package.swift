9679efec96f0f13b92f6f5d2cdcee513f43dbb52
Nicolas Märki <maerki@users.noreply.github.com>
2022-02-07 17:05:10 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -3,26 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppStoreConnectSDK",
+    name: "AppStoreConnect-Swift-SDK",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v11),
         .macOS(.v10_15)
     ],
     products: [
         // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]),
-        .library(name: "AppStoreConnectSDK", targets: ["AppStoreConnectSDK"])
+        .library(name: "AppStoreConnect-Swift-SDK", targets: ["AppStoreConnect-Swift-SDK"])
     ],
     dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
-
-        ],
+        // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.12.1"),
+        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/WeTransferPRLinter")
+    ],
     targets: [
         // dev .target(name: "DangerDependencies", dependencies: [
         // dev     .product(name: "Danger", package: "danger-swift"),
         // dev     .product(name: "WeTransferPRLinter", package: "WeTransferPRLinter")
         // dev ], path: "Submodules/WeTransfer-iOS-CI/DangerFakeSources", sources: ["DangerFakeSource.swift"]),
         // dev .testTarget(name: "AppStoreConnect-Swift-SDK-Tests", dependencies: ["AppStoreConnect-Swift-SDK"], path: "Tests", exclude: ["LinuxMain.swift"]),
-        .target(name: "AppStoreConnectSDK", dependencies: ["JWTKit"], path: "Sources")
+        .target(name: "AppStoreConnect-Swift-SDK", path: "Sources")
     ]
 )
